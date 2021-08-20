0b82ccfbf5d02c6b3f39efb5d13630ae81a8cd5a
KhaledAlabssi <khaled.absi5@gmail.com>
2021-08-20 12:46:41 +0200

    settingg up the app enviroment

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
+import path from 'path';
+
+
+const __dirname = path.resolve();
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts);
+app.use(express.static('public'))
+
+app.listen(process.env.PORT || 3000);
