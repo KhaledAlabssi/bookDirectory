93f4955bcda3900fb4bc01a7b7dbcc48c8d8629f
KhaledAlabssi <khaled.absi5@gmail.com>
2021-08-20 22:09:25 +0200

    change app type to normal and finlaize the app structure

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import expressLayouts from 'express-ejs-layouts';
-import path from 'path';
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index');
 
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -11,5 +12,6 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts);
 app.use(express.static('public'))
+app.use('/', indexRouter);
 
 app.listen(process.env.PORT || 3000);
